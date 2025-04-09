f96303159a40e4307909e0871bab7e6db1db5726
Junner1st <jimmy.zm.7219@gmail.com>
2025-04-09 15:13:27 +0800

    update AES-RSA chat room src

--- AES-RSA-encrypted [chat room]/src/Flask-Server-main/app.py
+++ AES-RSA-encrypted [chat room]/src/Flask-Server-main/app.py
@@ -1,11 +1,3 @@
-from flask import Flask, render_template, request
-from flask_socketio import SocketIO
-from include.crypt_functions import AESCipher, RSACipher
-import logging
-import base64
-import eventlet
-
-
 '''
 character: c_i, server
 js FUNCTION any_client_publish_rsa_key() -> INTEGER {
@@ -55,150 +47,79 @@ python FUNCTION server_forward_message_to_client_SINGLE( STRING text, STRING sid
 }
 '''
 
+from flask import Flask, request
+from flask_socketio import SocketIO, emit, send
+from flask_cors import CORS
+import logging
+import json
 
 app = Flask(__name__)
+CORS(app)
 socketio = SocketIO(app, cors_allowed_origins="*")
-
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s, %(levelname)s, %(message)s'
-)
-logger = logging.getLogger(__name__)
-
 
 clients = []
 
-reply_count_PUBLIC = 0
-
-reply_clients_list_single = []
-
-@socketio.on("client_request_join")
-def join_client():
+@socketio.on("connect")
+def handle_connect():
+    print(f"Client connected: {request.sid}")
     clients.append(request.sid)
-    logger.info(f"[joined] {request.sid} joined the server.")
+
+    user_list = [sid for sid in clients if sid != request.sid]
+    emit("connect_ack", {"user_list": user_list})
+    # print(f"Connected clients: {user_list}")
+
+@socketio.on("disconnect")
+def handle_disconnect():
+    print(f"Client disconnected: {request.sid}")
+    if request.sid in clients:
+        clients.remove(request.sid)
+        emit("user_left", {"sid": request.sid}, broadcast=True)
+
+@socketio.on("register")
+def handle_register(data):
+    rsa_key = data.get("rsa_key")
+
+    print(f"Client {request.sid} registered with RSA key.")
+
+
+    # Broadcast this client's RSA key to all others
+    print(f"current clients: {clients}")
     for sid in clients:
-        socketio.emit("server_sent_message", {"message": "joined", "client_sid": str(request.sid)}, room=sid)
+        if sid != request.sid:
+            print(f"Sending RSA key to {sid}")
+            emit("receive_rsa_key", {"sid": request.sid, "rsa_key": rsa_key}, to=sid)
 
+@socketio.on("send_aes_to_newcomer")
+def handle_send_encrypted_aes(data):
+    to_sid = data.get("target_sid")
+    encrypted_aes = data.get("encrypted_aes")
+    if to_sid in clients:
+        print(f"Sending AES key to {to_sid}")
+        emit("receive_aes_key", {"encrypted_aes": encrypted_aes}, to=to_sid)
+    else:
+        print(f"Target client {to_sid} not found.")
 
-
-@socketio.on("client_reply_recieved_PUBLIC")
-def listen_to_client_reply_ANY(data):
-    global reply_count_PUBLIC
-    logger.info(f"[message] Received message from {request.sid}")
-    logger.info(f"[message] Received message:{data.get('message')}")
-    reply_count += 1
-    socketio.emit("server_sent_message", {"message": "recieved"}, room=request.sid)
-    logger.info("[message] Sent acknowledgement to client.")
-
-@socketio.on("client_request_forward_message_ANY")
-def server_forward_message_to_client_ANY(data, sent_event="message"):
-    global reply_count
-
-    def forward_message():
-        for client_sid in clients:
-            if client_sid != request.sid:
-                socketio.emit(sent_event, data, room=client_sid)
-            logger.info(f"[{sent_event}] Forwarded message from {request.sid}")
-
-    def collect_replies():
-        eventlet.sleep(5)
-        logger.info(f"[replies] Received {reply_count} checked replies.")
-        reply_count = 0
-
-    eventlet.spawn(forward_message)
-    eventlet.spawn(collect_replies)
+@socketio.on("send_message")
+def handle_send_message(data):
+    payload = data.get("payload")
+    print(f"Received payload: {payload}")
+    if payload is None:
+        print("Invalid payload: payload is None")
+        return
+    try:
+        payload_dict = json.loads(payload)
+        message = payload_dict["data"]
+        iv = payload_dict["iv"]
+        print(f"Message: {message}")
+        print(f"IV: {iv}")
+    except (json.JSONDecodeError, KeyError) as e:
+        print("Invalid payload:", e)
+        return
     
 
-@socketio.on("client_reply_recieved_SINGLE")
-def listen_to_client_reply_SINGLE(data):
-    global reply_clients_list_single
-
-    logger.info(f"[message] Received message from {request.sid}")
-    logger.info(f"[message] Received message:{data.get('message')}")
-    reply_clients_list_single.append(request.sid)
-    socketio.emit("server_sent_message", {"message": "recieved"}, room=request.sid)
-    logger.info("[message] Sent acknowledgement to client.")
-
-@socketio.on("client_request_forward_message_SINGLE")
-def server_forward_message_to_client_SINGLE(data, sent_event="message"):
-    global reply_clients_list_single
-
-    sid = data.sid
-
-    def forward_message():
-        socketio.emit(sent_event, data, room=sid)
-        logger.info(f"[{sent_event}] Forwarded message to {request.sid}")
-
-    def collect_replies():
-        eventlet.sleep(5)
-        if sid in reply_clients_list_single:
-            logger.info(f"[replies] Received {sid} checked replies.")
-        else:
-            logger.info(f"[replies] Timeout for {sid} checked replies. [5s]")
-    
-    eventlet.spawn(forward_message)
-    eventlet.spawn(collect_replies)
-
-
-
-@app.route('/')
-def index():
-    return render_template("index.html")
-
-
-
-# @socketio.on("client_broadcast_rsa_public")
-# def broadcast_rsa_public():
-    
-
-# @socketio.on("client_request_rsa_public")
-# def send_rsa_public():
-#     socketio.emit(
-#         "server_sent_rsa_public_key",
-#         {"public_key": server_rsa_public.export_key().decode()}
-#         room=request.sid
-#     )
-
-# @socketio.on("client_sent_aes_key")
-# def receive_aes_key(data):
-#     global aes_key
-#     encrypted_aes_key = base64.b64decode(data.get("aes_key"))
-
-#     cipher_rsa = rsa_entity.cipher
-#     aes_key = cipher_rsa.decrypt(encrypted_aes_key)
-
-#     logger.info(f"[key exchange] AES Key:{aes_key}")
-    
-
-# @socketio.on("client_sent_message")
-# def handle_encrypted_message(data):
-#     global aes_key
-#     global aes_iv
-#     raw_message = data.get("message")
-#     logger.info(f"[client_sent_message] Received raw message:{raw_message}")
-
-#     ''' sent status to client '''
-#     socketio.emit("server_sent_message", {"message": "recieved"})
-#     logger.info("[message] Sent acknowledgement to client.")
-
-#     ''' extract the initialization vector; before ":" '''
-#     aes_iv = base64.b64decode(raw_message.split(":")[0])
-#     aes_encrypted_message = raw_message.split(":")[1]
-#     # aes_encrypted_message = aes_encrypted_message
-
-#     logger.info(f"[crypt] Received AES IV:{aes_iv}")
-#     logger.info(f"[message] Received AES encrypted message:{aes_encrypted_message}")
-
-#     ''' decrypt the message '''
-#     aes_entity.set_cipher(
-#         key=aes_key,
-#         iv=aes_iv
-#     )
-#     decrypted_message = aes_entity.decrypt_aes(aes_encrypted_message)
-
-#     logger.info(f"[crypt] Received message:{decrypted_message}")
+    print(f"Received message: {message} from {request.sid}")
+    emit("receive_message", {"sender": request.sid, "message": message, "iv": iv}, broadcast=True)
 
 
 if __name__ == "__main__":
-    logger.info("[server] Server started")
-    socketio.run(app, host="localhost", port=5055, debug=True)
+    socketio.run(app, port=5055)
