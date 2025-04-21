8372025f309e14460c659b01403394c81938daca
Junner1st <jimmy.zm.7219@gmail.com>
2025-04-21 14:57:30 +0800

    update scratch mod inv

--- side-practice/modular-multiplicative-inverse/scratch-modular-multiplicative-inverse.py
+++ side-practice/modular-multiplicative-inverse/scratch-modular-multiplicative-inverse.py
@@ -1,19 +1,21 @@
 def extended_euclidean(a, b):
     if a == 0:
-        return (b, 0, 1)
+        return b, 0, 1
     else:
-        g, y, x = extended_euclidean(b % a, a)
-        return (g, x - (b // a) * y, y)
+        gcd, x1, y1 = extended_euclidean(b % a, a)
+        x = y1 - (b // a) * x1
+        y = x1
+        return gcd, x, y
 
 def modinv(a, m):
-    g, x, y = extended_euclidean(a, m)
-    if g != 1:
+    gcd, x, _ = extended_euclidean(a, m)
+    if gcd != 1:
         raise Exception('modular inverse does not exist')
     else:
         return x % m
     
+if __name__ == "__main__":
+    a = 114
+    b = 58595
 
-a = 114
-b = 58595
-
-print("scratch modular multiplicative inverse: ", modinv(a, b))
+    print("modular multiplicative inverse:", modinv(a, b))
